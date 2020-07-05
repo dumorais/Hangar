d82fe6d503c028fb4f8dfc95a1252fcc3278b650
brdoliveira <66849496+brdoliveira@users.noreply.github.com>
2020-07-05 06:20:32 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
+
+function Bebidas() {
+  document.getElementById("card-bebidas").style.visibility = "visible";
+  document.getElementById("card-porcao").style.visibility = "hidden";
+}
+function Porcao() {
+  document.getElementById("card-bebidas").style.visibility = "hidden";
+  document.getElementById("card-porcao").style.visibility = "visible";
+}
