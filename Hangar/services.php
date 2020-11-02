22f6b9951e36e624c1e520636062ac32ba00784c
Eduardo Duarte Morais <eduartemorais@hotmail.com>
2020-11-02 20:40:03 -0300

    Excluindo pasta errada

--- Hangar/services.php
+++ Hangar/services.php
@@ -1,7 +1,7 @@
 <?php require_once 'conexao.php'; ?>
 
 <?php
-
+//
 function GetProdutos($idcategoria){
     $sql = "SELECT * FROM `produtos` WHERE idcategoria = $idcategoria ";
     $resultado=mysqli_query(GetMysql(), $sql);
